81028d67350c21b402bb8992ec37c2e71803fbf3
judge <tarasht@ce.sharif.edu>
2019-05-23 02:32:20 +0430

    Create accommodation html

--- Code/accommodation/forms.py
+++ Code/accommodation/forms.py
@@ -1,16 +1,20 @@
 from django import forms
 from .models import Accommodation, Amenity, Room
+
+
 # from .choices import AMENITY_CHOICES
 
 class AccommodationCreationForm(forms.ModelForm):
     class Meta:
         model = Accommodation
-        fields = ['accommodation_type', 'city', 'address', 'image']
+        fields = ['accommodation_type', 'title', 'description', 'city', 'address', 'image']
+
 
 class RoomCreationForm(forms.ModelForm):
     class Meta:
         model = Room
         fields = ['bed_type', 'number_of_guests']
+
 
 class AmenityForm(forms.ModelForm):
     class Meta:
--- Code/accommodation/models.py
+++ Code/accommodation/models.py
@@ -1,30 +1,41 @@
 from django.db import models
-from .choices import BED_TYPE_CHOICES,ACCOMMODATION_TYPE_CHOICES
+from .choices import BED_TYPE_CHOICES, ACCOMMODATION_TYPE_CHOICES
 from registration.models import Host
+
 
 # Create your models here.
 
 class Amenity(models.Model):
-    has_wifi = models.BooleanField(default = False)
-    has_tv = models.BooleanField(default = False)
-    has_washer = models.BooleanField(default = False)
-    has_warm_ac = models.BooleanField(default = False)
-    has_cool_ac = models.BooleanField(default = False)
-    has_parking = models.BooleanField(default = False)
-    has_kitchen = models.BooleanField(default = False)
-    has_utensils = models.BooleanField(default = False)
-    has_fridge = models.BooleanField(default = False)
-    has_oven = models.BooleanField(default = False)
-    has_bedsheets = models.BooleanField(default = False)
-    
+    has_wifi = models.BooleanField(default=False)
+    has_tv = models.BooleanField(default=False)
+    has_warm_ac = models.BooleanField(default=False)
+    has_cool_ac = models.BooleanField(default=False)
+    has_parking = models.BooleanField(default=False)
+    has_kitchen = models.BooleanField(default=False)
+    has_utensils = models.BooleanField(default=False)
+    has_fridge = models.BooleanField(default=False)
+    has_oven = models.BooleanField(default=False)
+    has_bedsheets = models.BooleanField(default=False)
+    has_bathroom = models.BooleanField(default=False)
+    has_shower = models.BooleanField(default=False)
+    has_tub = models.BooleanField(default=False)
+    has_toilet = models.BooleanField(default=False)
+    has_hairdryer = models.BooleanField(default=False)
+    has_roomservice = models.BooleanField(default=False)
+    has_laundry = models.BooleanField(default=False)
+    has_elevator = models.BooleanField(default=False)
+    has_safe = models.BooleanField(default=False)
+    has_iron = models.BooleanField(default=False)
+
 
 class Accommodation(models.Model):
     owner = models.ForeignKey(Host, related_name='owner', on_delete=models.CASCADE)
-    
-    accommodation_type = models.CharField(max_length = 20, choices = ACCOMMODATION_TYPE_CHOICES)
-    city = models.CharField(max_length = 20, default = 'تهران')
-    address = models.CharField(max_length = 100)
-    amenity = models.OneToOneField(Amenity, related_name = 'accommodation', on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    accommodation_type = models.CharField(max_length=20, choices=ACCOMMODATION_TYPE_CHOICES)
+    city = models.CharField(max_length=20, default='تهران')
+    address = models.CharField(max_length=100)
+    amenity = models.OneToOneField(Amenity, related_name='accommodation', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='../media/house_pics/', default='../media/house_pics/no-picture.png')
 
 
@@ -32,12 +43,7 @@ class Room(models.Model):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
     amenity = models.OneToOneField(Amenity, related_name='room', on_delete=models.CASCADE)
 
-    bed_type = models.CharField(max_length = 20, choices = BED_TYPE_CHOICES)
+    bed_type = models.CharField(max_length=20, choices=BED_TYPE_CHOICES)
     number_of_guests = models.IntegerField()
 
     how_many = models.IntegerField(default=1)
-
-
-
-
-
