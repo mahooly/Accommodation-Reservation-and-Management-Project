cca9cd7006c58965beb105388c38117e4e50d86e
Armin <arminbehnamnia@gmail.com>
2019-05-30 17:18:25 +0430

    allowed hosts added to settings.

--- Code/Code/settings.py
+++ Code/Code/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 't5e8^w_h%xp9m7#&iyyu55j8whfj^&-wokth#w)=fa7curyf^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['makan-system.herokuapp.com']
 
 
 # Application definition
