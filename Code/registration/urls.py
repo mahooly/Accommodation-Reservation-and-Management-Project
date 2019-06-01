d52b9dafa68b6a1475f13fdd6ee5977156dc2bea
judge <tarasht@ce.sharif.edu>
2019-06-01 16:42:10 +0430

    Password reset url change

--- Code/registration/urls.py
+++ Code/registration/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth import views as auth_views
 from registration import views
 
 urlpatterns = [
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', views.MainPageView.as_view(), name='home'),
     path('register/', views.RegistrationView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
