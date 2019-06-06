b143cc1eb7d949079cfd4fd074e7617051dede9d
Armin <arminbehnamnia@gmail.com>
2019-06-06 16:40:08 +0430

    problem with bash command being resolved.

--- Code/registration/tests/test_forms.py
+++ Code/registration/tests/test_forms.py
@@ -28,7 +28,7 @@ class TestRegistrationForm(TestCase):
         }
         form = CustomUserCreationForm(data=invalid_data)
         form.is_valid()
-        self.assertFalse(form.errors)
+        self.assertTrue(form.errors)
         invalid_data = {
             "username" : "armin_gm", "password1" : "1234", "password2" : "1234", "first_name" : "armin", "last_name" : "behnamnia"
         , "email" : "arminbehnamnia@gmail.com", "gender" : "male"
