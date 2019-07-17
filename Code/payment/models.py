99ad3a9957abae6d43fb2f44792d5ed00a8569ed
fatemeh.zardbani <fatemeh.zardbani@gmail.com>
2019-07-17 17:14:18 +0430

    opposite transaction

--- Code/payment/models.py
+++ Code/payment/models.py
@@ -6,9 +6,10 @@ class Transaction(models.Model):
     is_successful = models.BooleanField(default=False)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     creation_date = models.DateField(auto_now_add=True)
+    coefficient = models.IntegerField(default=1)
 
     @property
     def total_price(self):
         stay_len = self.reservation.check_out - self.reservation.check_in
         stay_len = stay_len.days
-        return self.reservation.roominfo.first().room.price * stay_len * self.reservation.roominfo.count()
+        return self.coefficient * self.reservation.roominfo.first().room.price * stay_len * self.reservation.roominfo.count()
